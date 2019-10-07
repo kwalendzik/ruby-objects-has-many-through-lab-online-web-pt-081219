f1e0fbd3f84fcccae350288497d0a1f78de3e568
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 03:59:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,14 @@ class Artist
   end
   
   def new_song(name, genre)
+    Song.new(name, genre)
+    Song.artist = self
+  end
   
+  def songs
+    Songs.all.select {|song| song.artist == self}
+  end
+  
+  def genres
   
 end
