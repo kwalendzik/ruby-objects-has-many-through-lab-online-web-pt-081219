b863a96799227f06aa80669e5e72b11fa6ee53a4
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 04:20:41 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointment
+    Appointment.all.select {|appointment| appointment.doctor
+  
+  
+  
+  
+end
