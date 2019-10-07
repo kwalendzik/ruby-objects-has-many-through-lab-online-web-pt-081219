f54abe38d39c5b8e9b634bd65c2e2d39675430fd
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 03:55:03 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+  
+  
+end
